8455527becd9f5d2fd4204bcf4490410f07fbcda
myeonghan-nim <ataraxia9408@gmail.com>
2021-08-05 22:03:28 +0900

    Change: app name

--- qna/urls.py
+++ qna/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('questions/', include('articles.urls')),
+    path('question/', include('question.urls')),
 ]
--- question/views.py
+++ question/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+
 from .models import Question, Answer
 
 
